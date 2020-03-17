c32f3f3260e2bdd004b6f91abd9100474ffda56e
Anusree1999 <62303784+Anusree1999@users.noreply.github.com>
2020-03-18 00:00:20 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,29 @@
+import time
+class learn:
+    number_of_participant=input("number of interested individuals:")
+    number_of_participant=int(number_of_participant)
+    for i in range(1,number_of_participant):
+        name=""
+        interests=""
+        t=0
+        choice=""
+        def __init__(a):
+            a
+        def addStacks(self,interest):
+            self.interest=interest
+            setMentorOrLearner(choice)
+        def setAvailableTime():
+            time=input("enter a time in the format HH:MM \n")
+            h,m=map(int,time.split(":"))
+            t=h,m
+            return t
+            #print time
+        def setMentorOrLearner(self,choice):
+            self.choice=choice
+            if learn.choice=="mentor":
+                setAvailableTime()
+                print(t)
+        def getMentor(stack,time):
+            self.stack=stack
+            self.time=time
+r=learn()
